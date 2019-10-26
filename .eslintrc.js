3bdee59aebdc9c54b11e724a8a3c048f2cabd10e
Reaper622 <1064416560@qq.com>
2019-10-26 16:03:05 +0800

    :pencil2: chore: eslint 配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,16 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 不加分号
+    'semi': ['error', 'never'],
+    // 驼峰命名
+    'camelcase': 1,
+    // 缩进为 tab
+    'indent': ['error', 'tab'] ,
+    // 引号使用单引号
+    'quotes': ['error', 'single'],
+    'no-tabs': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
